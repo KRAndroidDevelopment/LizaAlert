37c81eef81bc5b55e62aea011b5ecd761430bd11
RomanKorolev <roman-korolev2007@narod.ru>
2017-01-30 20:41:17 +0400

    added xml load

--- src/com/lizaalert/MyService.java
+++ src/com/lizaalert/MyService.java
@@ -1,5 +1,10 @@
 package com.lizaalert;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -13,7 +18,53 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-    	Log.d(TAG, "MyService::onStartCommand flags="+flags+" startId="+startId);
+    	if(intent == null){
+        	Log.d(TAG, "MyService::onStartCommand intent=null flags="+flags+" startId="+startId);
+    	}else{
+        	Log.d(TAG, "MyService::onStartCommand intent="+(intent.toString())+"flags="+flags+" startId="+startId);
+    	}
+		new Thread(new Runnable() {
+	    	@Override
+	    	public void run() {
+	    		while(true){
+		    		String xml = check_updates();
+		    		if(xml != null){
+		    			
+		    		}
+		    		try {
+						//Thread.sleep(1000 * 10);
+						Thread.sleep(1000L * 60L * 5L);
+					} catch (InterruptedException e) {
+						break;
+					}
+	    		}
+	    	}
+	    	
+	    	protected String check_updates(){
+	    		try {
+	    	        URL url = new URL("http://narod-fl.ru/lost_humans.xml");
+	    	        URLConnection conn = url.openConnection();
+	    	        BufferedReader reader = new BufferedReader(
+	    	                new InputStreamReader(conn.getInputStream()));
+	    			StringBuilder sb = new StringBuilder();
+	    	        char [] buf = new char[128];
+	    	        while ( true ){
+	    	        	int c = reader.read(buf);
+	    	        	if(c == -1){ 
+	    	        		break;
+	    	        	}
+	    	            sb.append(buf, 0, c);
+	    	        }
+	    	        reader.close();
+	    			//Log.d("myLogs", sb.toString());
+	    			return sb.toString();
+	    		} catch (Exception e) {
+	    			Log.e("myLogs", e.toString());
+	    		}
+	    		return null;
+	    	}
+	    }).start();
+    	
         return Service.START_STICKY; //restarts every 7 seconds
         //return Service.START_NOT_STICKY;
     	//return Service.START_REDELIVER_INTENT;
